7dbc295be559dab63e68dc404e1b6fb5638e378b
Prem Tatkari <tatkariprem2002@gmail.com>
2023-04-17 23:24:56 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,174 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+
+mongoose.set({strictQuery: true});
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {useNewUrlParser: true});
+
+const userSchema = mongoose.Schema({
+    email: String,
+    password: String,
+    googleId: String,
+    secret: String 
+});
+
+const eventSchema = mongoose.Schema({
+    Title: String,
+    Description: String,
+    ImageURL: String,
+    Preference: Array
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = mongoose.model("User", userSchema);
+const Event = mongoose.model("Event", eventSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      cb(null, { id: user.id, username: user.username });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/auth/google", 
+    passport.authenticate("google", {scope: ["profile"]})
+);
+
+app.get("/auth/google/secrets", 
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home or other special page.
+    res.redirect('/preferences');
+  });
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.get("/preferences", function(req, res){
+    res.render("preferences");
+});
+
+app.get("/submit", function(req, res){
+    if(req.isAuthenticated()){
+        res.render("submit");
+    } else{
+        res.redirect("/login");
+    }
+});
+
+app.get("/logout", function(req, res){
+    req.logout(function(err){
+        if(err){ 
+            console.log(err); 
+        }
+    });
+    res.redirect("/");
+});
+
+app.post("/register", function(req, res){
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        } else{
+            passport.authenticate("local") (req, res, function(){
+                res.redirect("/preferences");
+            });
+        }
+    });
+});
+
+app.post("/login", function(req, res){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+            res.redirect("/login");
+        } else{
+            passport.authenticate("local") (req, res, function(){
+                res.redirect("/preferences");
+            });
+        }
+    });
+});
+
+app.post("/submit", function(req, res){
+    const checks = req.body.preferences;
+    console.log(checks);
+
+    Event.find({ Preference: { $all: checks} }, function(err, events) {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            if(events) {
+                res.render("submit", {events: events});
+            }
+        }
+    })
+});
+
+
+
+app.listen(3000, function(){
+    console.log("Server running on port 3000");
+});
